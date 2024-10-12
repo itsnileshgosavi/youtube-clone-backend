07e4a8f2ea49b48ae67afd4e2c4e150bc21aed9c
Nilesh <nbgosavi5@gmail.com>
2024-10-12 21:53:39 +0530

    changed cookie type

--- src/controller/user.signin.js
+++ src/controller/user.signin.js
@@ -30,6 +30,7 @@ export const signin = async (req, res) => {
             sameSite: "none",
             credentials: true,
             secure: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         });
         //sending success response
         res.status(200).json({
