d6f74db09b9da23063758dcfea8801c3d2c216c4
Nilesh <nbgosavi5@gmail.com>
2024-10-12 16:27:51 +0530

    fixed bug

--- src/controller/user.get.js
+++ src/controller/user.get.js
@@ -7,7 +7,14 @@ export const getUser = async (req, res) => {
         if (!user) {
             return res.status(404).json({ success: false, message: "User not found" }); //if the user is not found sending 404
         }
-        res.status(200).json({ success: true, user:{...user, password: undefined} }); //sending the user
+        res.status(200).json({ success: true, user:{_id:user._id,
+            firstName:user.firstName,
+            lastName:user.lastName,
+            email:user.email,
+            avatar:user.avatar,
+            createdAt:user.createdAt,
+            channels:user.channels}
+         }); //sending the user
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
     }
