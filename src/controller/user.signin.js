7a1f079d0048068f32864071b424b83d4b7728df
Nilesh <nbgosavi5@gmail.com>
2024-10-12 14:34:14 +0530

    updated cookie setting

--- src/controller/user.signin.js
+++ src/controller/user.signin.js
@@ -31,6 +31,7 @@ export const signin = async (req, res) => {
             secure: false,
             path: "/",
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days 
+            credentials: true,
         });
         //sending success response
         res.status(200).json({
